a21badad8471adf2e5a8ae6237b204a144077540
am999 <am9999072080@gmail.com>
2023-07-14 21:45:04 +0300

    Commit_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String s = "madam";
+        boolean isPalindrome = isPalindrome(s);
+        System.out.println(isPalindrome); // true
+    }
+
+    public static boolean isPalindrome(String s) {
+        int length = s.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (s.charAt(i) != s.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
